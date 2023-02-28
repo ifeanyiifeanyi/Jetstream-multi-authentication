1bd699c07b61cf9c924a4dddeddb8ffeb1bc9a3e
Ifeanyi Nnaemego <josephifeanyid2@gmail.com>
2023-02-28 15:52:24 +0100

    visa offer showing

--- app/Http/Controllers/Users/LatestVisaController.php
+++ app/Http/Controllers/Users/LatestVisaController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Users;
 
 use App\Http\Controllers\Controller;
+use App\Models\Visa;
 use Illuminate\Http\Request;
 
 class LatestVisaController extends Controller
 {
     public function index()
     {
-        return view('profile.applications.visas.index');
+        $visas = Visa::latest()->get();
+        // dd($visas);
+
+        return view('profile.applications.visas.index', ['visas' => $visas]);
     }
 }
